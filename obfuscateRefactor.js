834a55527b02d436e2ebfb54f87266b09057be85
Kendall Rowe <kendall.rowe312gmail.com>
2019-06-25 21:56:38 +0000

    Linted formatting of function and semicolons

--- obfuscateRefactor.js
+++ obfuscateRefactor.js
@@ -5,7 +5,7 @@ const obfuscate = function(inputString) {
   const replaceableLetters = ["a", "e", "o", "l"];
   
   // Loop through each letter of the input string
-  for (let i = 0; i < inputString.length;i++) {
+  for (let i = 0; i < inputString.length; i++) {
     currentLetter = inputString.charAt(i);
     // Check whether current letter is any of the replaceable letters and apply function to replace if so
     for (let j = 0; j < replaceableLetters.length; j++) {
@@ -18,16 +18,26 @@ const obfuscate = function(inputString) {
 
 // Check if current inputted letter matches one of our rules, replace if so
 const checkLetters = {
-  a: function(inputLetter) { return replaceLetter(inputLetter, "a", "4") },
-  e: function(inputLetter) { return replaceLetter(inputLetter, "e", "3") },
-  o: function(inputLetter) { return replaceLetter(inputLetter, "o", "0") },
-  l: function(inputLetter) { return replaceLetter(inputLetter, "l", "1") },
-}
+  a: function(inputLetter) {
+    return replaceLetter(inputLetter, "a", "4");
+  },
+  e: function(inputLetter) {
+    return replaceLetter(inputLetter, "e", "3");
+  },
+  o: function(inputLetter) {
+    return replaceLetter(inputLetter, "o", "0");
+  },
+  l: function(inputLetter) {
+    return replaceLetter(inputLetter, "l", "1");
+  }
+};
 
 const replaceLetter = function(inputString, checkString, replacementString) {
-  if (inputString === checkString) {return replacementString}
+  if (inputString === checkString) {
+    return replacementString;
+  }
   return inputString;
-}
+};
 
 console.log(obfuscate("Kendall"));
 console.log(obfuscate("password"));
